d0fb681c85fa728ef3fe3711643ba6fafee8cc4d
Gustavo Araya <galfaru99@gmail.com>
2023-04-22 00:11:34 -0400

    fix: Fixes css not working (changes static files)

--- FinanceManager/mainApp/views.py
+++ FinanceManager/mainApp/views.py
@@ -4,5 +4,5 @@ from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('index/index.html')
+    template = loader.get_template('./index.html')
     return HttpResponse(template.render())

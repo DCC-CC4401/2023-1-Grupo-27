2971b453b9e557cd34a2e02137613c0e33d78414
Jonathan <jonathandrp@outlook.es>
2023-04-22 07:46:19 -0400

    refactor: added possible functionality with comments

--- FinanceManager/mainApp/models.py
+++ FinanceManager/mainApp/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     saldo = models.IntegerField(default=0)
+    #apodo = models.CharField(max_length=30)
+    #pronombres = [('La','La'),('El','El'), ('Le','Le'),('Otro','Otro')]
+    #pronombre = models.CharField(max_length=5,choices=pronombres)

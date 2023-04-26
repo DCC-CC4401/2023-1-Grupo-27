e39af68150f56504bf6087b12bafecc9008ca41b
Yiruzz <arturofks@gmail.com>
2023-04-26 12:14:27 -0400

    Fix: Change the DB to only have 2 tables instead of 3, there is no longer a RealizaTransaccion model

--- FinanceManager/mainApp/admin.py
+++ FinanceManager/mainApp/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from mainApp.models import User
 from mainApp.models import Transaccion
-from mainApp.models import RealizaTransaccion
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Transaccion)
-admin.site.register(RealizaTransaccion)

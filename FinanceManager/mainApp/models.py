64660380fd2ee5952d87c58a1f18dac27b4799df
Yiruzz <arturofks@gmail.com>
2023-04-30 10:11:01 -0400

    Doc: Added comments to some views.py methods, also changed minor style features to index.html

--- FinanceManager/mainApp/models.py
+++ FinanceManager/mainApp/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
-class User(AbstractUser):
-    saldo = models.IntegerField(default=0)
+class User(AbstractUser): # Subclase de AbstractUser
+    saldo = models.IntegerField(default=0) # Agregamos un atributo de saldo al usuario, por defecto es 0
 
-class Transaccion(models.Model):  # Django añade un id por defecto
-    nombre = models.CharField(max_length=250)  # un varchar
-    tipos = [('Ingreso','Ingreso'),('Egreso','Egreso')] 
-    tipo = models.CharField(max_length=8,choices=tipos) # Tipo = ingreso/egreso
+class Transaccion(models.Model):  # Django añade un id por defecto, no es necesario definir llave
+    nombre = models.CharField(max_length=250)  # Nombre de la transaccion
+    tipos = [('Ingreso','Ingreso'),('Egreso','Egreso')] # Tipo de la transaccion (ingreso o egreso)
+    tipo = models.CharField(max_length=8,choices=tipos)
     categoria = models.CharField(max_length=250, default=None, null=True) # Categoria que es null por defecto
-    monto = models.IntegerField() # Un monto requerido de asignar
+    monto = models.IntegerField() # Un monto de la transaccion requerido de asignar
     fecha = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))  # Un date de cuando se crea una transaccion
     usuario = models.ForeignKey(User, on_delete=models.CASCADE) # Llave foranea a usuario
 
-    def __str__(self): # Como se mostrara al imprimir
+    def __str__(self): # Como se mostrara al imprimir (como un toString() de Java)
         return f'Usuario: {self.usuario}, Transaccion: {self.nombre} ({self.categoria}): {self.monto}'
     
 
--- FinanceManager/mainApp/views.py
+++ FinanceManager/mainApp/views.py
@@ -3,31 +3,28 @@ from django.db import IntegrityError
 from django.db import models
 from mainApp.models import User
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.template import loader
 from django.contrib.auth import authenticate, login, logout
 from .models import Transaccion
-from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 
-def index(request):
-    if request.method == "GET":
+def index(request):  # Metodo para la pagina principal
+    if request.method == "GET": # Se esta cargando la pagina
         user = request.user # Obtenemos el usuario
         transacciones = Transaccion.objects.filter(usuario=user.id) # Obtener transacciones del usuario
         saldo = saldo_usuario(user.id) # Obtener saldo del usuario
-        return render(request, "index.html", {"transacciones": transacciones, "user": user, "saldo": saldo})
-    if request.method == "POST":
-        if "ingreso" in request.POST or "egreso" in request.POST:
-            tipo = "Ingreso" if "ingreso" in request.POST else "Egreso"
+        return render(request, "index.html", {"transacciones": transacciones, "user": user, "saldo": saldo}) # Cargamos la pagina con lo correspondiente
+    if request.method == "POST": # Se envía un formulario desde la pagina
+        if "ingreso" in request.POST or "egreso" in request.POST: # Estamos recibiendo un post del modal de ingreso o egreso
+            tipo = "Ingreso" if "ingreso" in request.POST else "Egreso" # Diferenciamos caso entre ingreso o egreso
             titulo = request.POST["titulo"]  # titulo de la transaccion
             nombre_categoria = request.POST["categoria"]  # Nombre de la categoria, en caso de que no se especifique sera el string vacio por defecto
             monto = request.POST["monto"]  # Monto del ingreso
             fecha = request.POST["fecha"] # Fecha de la transaccion
             if request.user.is_authenticated: # Creamos la transaccion con los datos obtenidos si el usuario esta autenticado
-                if fecha != "": # Caso de que no se ingrese una fecha
+                if fecha != "": # Caso de que se ingrese una fecha
                     nueva_transaccion = Transaccion(nombre=titulo, tipo=tipo, categoria=nombre_categoria, monto=monto, fecha=fecha, usuario=request.user) 
-                else:
+                else: # Caso en que no se haya ingresado una fecha
                     nueva_transaccion = Transaccion(nombre=titulo, tipo=tipo, categoria=nombre_categoria, monto=monto, usuario=request.user) 
                 nueva_transaccion.save()  # Se guarda la transaccion en base de datos
         if "modificar" in request.POST:
@@ -55,46 +52,41 @@ def index(request):
                 transaccion.delete()
     return redirect("/mainApp")
 
-def register_user(request):
+def register_user(request): # Metodo para registrar al usuario
     if request.method == 'GET': #Si estamos cargando la página
         return render(request, "./register_user.html") #Mostrar el template
 
     elif request.method == 'POST': #Si estamos recibiendo el form de registro
         #Tomar los elementos del formulario que vienen en request.POST
-        nombre = request.POST['nombre']
-        contraseña = request.POST['contraseña']
-        #apodo = request.POST['apodo']
-        #pronombre = request.POST['pronombre']
-        #mail = request.POST['mail']
+        nombre = request.POST['nombre'] # Guardamos el nombre de usuario
+        contraseña = request.POST['contraseña'] # Guardamos la contraseña del usuario
 
-        #Crear el nuevo usuario
-        #user = User.objects.create_user(username=nombre, password=contraseña, email=mail, apodo=apodo, pronombre=pronombre)
-        try:
-            user = User.objects.create_user(username=nombre, password=contraseña)
-            login(request,user)
+        try: # Intentamos crear un nuevo usuario
+            user = User.objects.create_user(username=nombre, password=contraseña) #Creamos un nuevo usuario con los datos recien obtenidos 
+            login(request,user) # Logeamos al usuario recien creado
             #Redireccionar la página /index
             return HttpResponseRedirect('/mainApp')
-        except IntegrityError: # Caso en que el usuario con tal nombre ya exista
+        except IntegrityError: # Caso en que el usuario con tal nombre ya exista, recargamos la pagina
             return HttpResponseRedirect('.')
 
 
 
-def login_user(request):
-    if request.method == 'GET':
-        return render(request,"./login.html")
-    if request.method == 'POST':
+def login_user(request): # Metodo para hacer login a un usuario
+    if request.method == 'GET': # Si estamos cargando la pagina
+        return render(request,"./login.html") # Cargamos el html
+    if request.method == 'POST': # Cuando se envía algo de la pagina (form)
         username = request.POST['username'] # Obtenemos nombre de usuario
         contraseña = request.POST['contraseña'] # Obtenemos su contraseña
         usuario = authenticate(username=username,password=contraseña) # Autenticamos el usuario
         if usuario is not None: # Si la respuesta de la autenticacion no fue None, el usuario existe y puede entrar
-            login(request,usuario)
-            return HttpResponseRedirect('../mainApp')
-        else: # Caso contrario debe registrarse
-            return HttpResponseRedirect('/login')
+            login(request,usuario) # Se logea el usuario
+            return HttpResponseRedirect('../mainApp') # Ingresamos a la pagina principal
+        else: # Caso contrario debe registrarse, o bien se equivoco al ingresar datos
+            return HttpResponseRedirect('/login') # Recargamos la pagina
         
-def logout_user(request):
-    logout(request)
-    return redirect("/mainApp")
+def logout_user(request): # Metodo para hacer logout de un user
+    logout(request) # Hacemos logout del usuario
+    return redirect("/mainApp") # Redireccionamos a la pagina de inicio
 
 def saldo_usuario(id_usuario):
     if id_usuario is not None:
